20013109577526a65341fda043b59e7f649ea3db
deezone <darrendouglas.lee@gmail.com>
2021-01-09 18:49:22 -0500

    Issue-13 refactor api to use configuration package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// Core packages
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,7 +13,9 @@ import (
 
 	// Internal packages
 	"github.com/deezone/HydroBytes-BaseStation/cmd/api/internal/handlers"
+	"github.com/deezone/HydroBytes-BaseStation/internal/platform/conf"
 	"github.com/deezone/HydroBytes-BaseStation/internal/platform/database"
+
 )
 
 // Main entry point for program.
@@ -20,11 +23,38 @@ func main() {
 
 	// Define constants that includes their type to ensure a "kind" decarlation is not used resulting in much
 	// larger memory space use. See https://education.ardanlabs.com/courses/take/ultimate-syntax/lessons/13570526-constants-pt-2
-	const (
-		readTimeout time.Duration   = 5 * time.Second
-		writeTimeout time.Duration  = 5 * time.Second
-		cancelTimeout time.Duration = 5 * time.Second
-	)
+	// ex: readTimeout time.Duration   = 5 * time.Second
+
+	// =========================================================================
+	// Configuration
+
+	var cfg struct {
+		Web struct {
+			Address         string        `conf:"default:localhost:8000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:localhost"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+	}
+
+	if err := conf.Parse(os.Args[1:], "STATIONS", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("STATIONS", &cfg)
+			if err != nil {
+				log.Fatalf("error : generating config usage : %v", err)
+			}
+			fmt.Println(usage)
+			return
+		}
+		log.Fatalf("error: parsing config: %s", err)
+	}
 
 	// =========================================================================
 	// App Starting
@@ -32,10 +62,22 @@ func main() {
 	log.Printf("main : Started")
 	defer log.Println("main : Completed")
 
+	out, err := conf.String(&cfg)
+	if err != nil {
+		log.Fatalf("error : generating config for output : %v", err)
+	}
+	log.Printf("main : Config :\n%v\n", out)
+
 	// =========================================================================
 	// Start Database
 
-	db, err := database.Open()
+	db, err := database.Open(database.Config{
+		User:       cfg.DB.User,
+		Password:   cfg.DB.Password,
+		Host:       cfg.DB.Host,
+		Name:       cfg.DB.Name,
+		DisableTLS: cfg.DB.DisableTLS,
+	})
 	if err != nil {
 		log.Fatalf("error: connecting to db: %s", err)
 	}
@@ -61,10 +103,10 @@ func main() {
 	 * https://golang.org/src/net/http/server.go?s=97511:97566#L3108
 	 */
 	api := http.Server{
-		Addr:         "localhost:8000",
+		Addr:         cfg.Web.Address,
 		Handler:      http.HandlerFunc(stationTypeHandler.List),
-		ReadTimeout:  readTimeout,
-		WriteTimeout: writeTimeout,
+		ReadTimeout:  cfg.Web.ReadTimeout,
+		WriteTimeout: cfg.Web.WriteTimeout,
 	}
 
 	// Make a channel to listen for errors coming from the listener. Use a buffered channel so the goroutine can exit
@@ -106,13 +148,13 @@ func main() {
 		 * processes.
 		 * https://golang.org/pkg/context/
 		 */
-		ctx, cancel := context.WithTimeout(context.Background(), cancelTimeout)
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.Web.ShutdownTimeout)
 		defer cancel()
 
 		// Asking listener to shutdown and load shed.
 		err := api.Shutdown(ctx)
 		if err != nil {
-			log.Printf("main : Graceful shutdown did not complete in %v : %v", cancelTimeout, err)
+			log.Printf("main : Graceful shutdown did not complete in %v : %v", cfg.Web.ShutdownTimeout, err)
 			err = api.Close()
 		}
 
