a58404758325c0c4b4234fef7cbf6c3e231477ec
deezone <darrendouglas.lee@gmail.com>
2021-01-13 21:36:00 -0500

    Issue-25 impliment RespondError()

--- internal/platform/web/response.go
+++ internal/platform/web/response.go
@@ -4,13 +4,15 @@ import (
 	// Core packages
 	"encoding/json"
 	"net/http"
+
+	// Third party packages
+	"github.com/pkg/errors"
 )
 
 // Respond converts a Go value to JSON and sends it to the client.
 func Respond(w http.ResponseWriter, data interface{}, statusCode int) error {
 
 	// Convert the response value to JSON.
-	// https://golang.org/pkg/encoding/json/#Marshal
 	res, err := json.Marshal(data)
 	if err != nil {
 		return err
@@ -19,11 +21,34 @@ func Respond(w http.ResponseWriter, data interface{}, statusCode int) error {
 	// Respond with the provided JSON.
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(statusCode)
-
-	// https://golang.org/pkg/net/http/#Request.Write
 	if _, err := w.Write(res); err != nil {
 		return err
 	}
 
+	return nil
+}
+
+// RespondError sends an error reponse back to the client.
+func RespondError(w http.ResponseWriter, err error) error {
+
+	// If the error was of the type *Error, the handler has
+	// a specific status code and error to return.
+	if webErr, ok := errors.Cause(err).(*Error); ok {
+		er := ErrorResponse{
+			Error: webErr.Err.Error(),
+		}
+		if err := Respond(w, er, webErr.Status); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// If not, the handler sent any arbitrary error value so use 500.
+	er := ErrorResponse{
+		Error: http.StatusText(http.StatusInternalServerError),
+	}
+	if err := Respond(w, er, http.StatusInternalServerError); err != nil {
+		return err
+	}
 	return nil
 }
