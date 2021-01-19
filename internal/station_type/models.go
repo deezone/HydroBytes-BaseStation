2fcd82434259e1bccc43c3fedb9f2ddbf3f4e6d0
deezone <darrendouglas.lee@gmail.com>
2021-01-18 20:01:00 -0500

    Issue-37 add update models for both StationType and Station

--- internal/station_type/models.go
+++ internal/station_type/models.go
@@ -29,6 +29,17 @@ type NewStationType struct {
 	Description string    `json:"description"`
 }
 
+// UpdateStationType defines what information may be provided to modify an
+// existing StationType. All fields are optional so clients can send just the
+// fields they want changed. It uses pointer fields so we can differentiate
+// between a field that were not provided and a field that was provided as
+// explicitly blank. Normally we do not want to use pointers to basic types but
+// we make exceptions around marshalling/unmarshalling.
+type UpdateStationType struct {
+	Name         *string `json:"name"`
+	Description  *string `json:"description"`
+}
+
 // Station is a station that is defined as one of the station types in StationType.
 type Station struct {
 	Id            string    `db:"id"              json:"id"`
@@ -47,4 +58,17 @@ type NewStation struct {
 	Description   string    `db:"description"     json:"description"`
 	LocationX     int       `db:"location_x"      json:"location_x" validate:"required,gte=0"`
 	LocationY     int       `db:"location_y"      json:"location_y" validate:"required,gte=0"`
+}
+
+// UpdateStation defines what information may be provided to modify an
+// existing Station. All fields are optional so clients can send just the
+// fields they want changed. It uses pointer fields so we can differentiate
+// between a field that were not provided and a field that was provided as
+// explicitly blank. Normally we do not want to use pointers to basic types but
+// we make exceptions around marshalling/unmarshalling.
+type UpdateStation struct {
+	Name         *string `json:"name"`
+	Description  *string `json:"description"`
+	LocationX    *int    `json:"location_x" validate:"omitempty,gte=0"`
+	LocationY    *int    `json:"location_y" validate:"omitempty,gte=0"`
 }
