a8df2c69f41fab6d4a0edf9955312ffcb70a65f8
deezone <darrendouglas.lee@gmail.com>
2021-01-18 19:59:43 -0500

    Issue-37 addition of update routes

--- cmd/api/internal/handlers/routes.go
+++ cmd/api/internal/handlers/routes.go
@@ -19,12 +19,14 @@ func API(db *sqlx.DB, log *log.Logger) http.Handler {
 
 	st := StationType{db: db, log: log}
 
-	app.Handle(http.MethodGet, "/v1/station-types", st.List)
-	app.Handle(http.MethodGet, "/v1/station-type/{id}", st.Retrieve)
+	app.Handle(http.MethodGet,  "/v1/station-types", st.List)
+	app.Handle(http.MethodGet,  "/v1/station-type/{id}", st.Retrieve)
 	app.Handle(http.MethodPost, "/v1/station-type", st.Create)
+	app.Handle(http.MethodPut,  "/v1/station-type/{id}", st.Update)
 
 	app.Handle(http.MethodPost, "/v1/station-type/{id}/station", st.AddStation)
-	app.Handle(http.MethodGet, "/v1/station-type/{id}/stations", st.ListStations)
+	app.Handle(http.MethodGet,  "/v1/station-type/{id}/stations", st.ListStations)
+	app.Handle(http.MethodGet,  "/v1/station/{id}", st.AdjustStation)
 
 	return app
 }
