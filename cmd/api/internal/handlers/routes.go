9ea70925d231663d2a18d318e02d16204071243c
deezone <darrendouglas.lee@gmail.com>
2021-01-23 21:25:32 -0500

    Issue-40 refactor to include Check connection and health route

--- cmd/api/internal/handlers/routes.go
+++ cmd/api/internal/handlers/routes.go
@@ -17,19 +17,27 @@ func API(db *sqlx.DB, log *log.Logger) http.Handler {
 
 	app := web.NewApp(log)
 
-	st := StationType{db: db, log: log}
+	{
+		c := Check{db: db}
 
-	app.Handle(http.MethodGet,    "/v1/station-types", st.List)
-	app.Handle(http.MethodGet,    "/v1/station-type/{id}", st.Retrieve)
-	app.Handle(http.MethodPost,   "/v1/station-type", st.Create)
-	app.Handle(http.MethodPut,    "/v1/station-type/{id}", st.Update)
-	app.Handle(http.MethodDelete, "/v1/station-type/{id}", st.Delete)
+		app.Handle(http.MethodGet, "/v1/health", c.Health)
+	}
 
-	app.Handle(http.MethodGet,    "/v1/station-type/{id}/stations", st.ListStations)
-	app.Handle(http.MethodGet,    "/v1/station/{id}", st.RetrieveStation)
-	app.Handle(http.MethodPost,   "/v1/station-type/{id}/station", st.AddStation)
-	app.Handle(http.MethodPut,    "/v1/station/{id}", st.AdjustStation)
-	app.Handle(http.MethodDelete, "/v1/station/{id}", st.DeleteStation)
+	{
+		st := StationType{db: db, log: log}
+
+		app.Handle(http.MethodGet,    "/v1/station-types",     st.List)
+		app.Handle(http.MethodGet,    "/v1/station-type/{id}", st.Retrieve)
+		app.Handle(http.MethodPost,   "/v1/station-type",      st.Create)
+		app.Handle(http.MethodPut,    "/v1/station-type/{id}", st.Update)
+		app.Handle(http.MethodDelete, "/v1/station-type/{id}", st.Delete)
+
+		app.Handle(http.MethodGet,    "/v1/station-type/{id}/stations", st.ListStations)
+		app.Handle(http.MethodGet,    "/v1/station/{id}",               st.RetrieveStation)
+		app.Handle(http.MethodPost,   "/v1/station-type/{id}/station",  st.AddStation)
+		app.Handle(http.MethodPut,    "/v1/station/{id}",               st.AdjustStation)
+		app.Handle(http.MethodDelete, "/v1/station/{id}",               st.DeleteStation)
+	}
 
 	return app
 }
