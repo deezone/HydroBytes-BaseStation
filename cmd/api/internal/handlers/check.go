02e6aed0fef34f9e4402a1c47405e12c067667f1
deezone <darrendouglas.lee@gmail.com>
2021-01-25 19:08:52 -0500

    Issue-48 addition of content to each of the handler signatures

--- cmd/api/internal/handlers/check.go
+++ cmd/api/internal/handlers/check.go
@@ -31,10 +31,9 @@ func (c *Check) Health(w http.ResponseWriter, r *http.Request) error {
 		// status. Do not respond by just returning an error because further up in
 		// the call stack will interpret that as an unhandled error.
 		health.Status = "db not ready"
-		return web.Respond(w, health, http.StatusInternalServerError)
+		return web.Respond(r.Context(), w, health, http.StatusInternalServerError)
 	}
 
 	health.Status = "ok"
-	return web.Respond(w, health, http.StatusOK)
+	return web.Respond(r.Context(), w, health, http.StatusOK)
 }
-
