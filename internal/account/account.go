112cb4517bd02b1dca07d96523af7c0e27d0447f
deezone <darrendouglas.lee@gmail.com>
2021-01-27 21:57:51 -0500

    Issue-54 addition of Authenticate business logic

--- internal/account/account.go
+++ internal/account/account.go
@@ -3,7 +3,11 @@ package account
 import (
 	// Core packages
 	"context"
+	"database/sql"
 	"time"
+
+	// Internal packages
+	"github.com/deezone/HydroBytes-BaseStation/internal/platform/auth"
 
 	// Third-party packages
 	"github.com/google/uuid"
@@ -11,6 +15,45 @@ import (
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var (
+	// ErrAuthenticationFailure occurs when an account attempts to authenticate but something goes wrong.
+	ErrAuthenticationFailure = errors.New("Authentication failed")
+)
+
+/**
+ * Authenticate finds an account by the name (unique) and verifies the password. On
+ * success it returns a Claims value representing the account. The claims can be
+ * used to generate a token for future authentication.
+ */
+
+func Authenticate(ctx context.Context, db *sqlx.DB, now time.Time, name, password string) (auth.Claims, error) {
+
+	const q = `SELECT * FROM account WHERE name = $1`
+
+	var a Account
+	if err := db.GetContext(ctx, &a, q, name); err != nil {
+
+		// Normally we would return ErrNotFound in this scenario but we do not want
+		// to leak to an unauthenticated user which emails are in the system.
+		if err == sql.ErrNoRows {
+			return auth.Claims{}, ErrAuthenticationFailure
+		}
+
+		return auth.Claims{}, errors.Wrap(err, "selecting single account")
+	}
+
+	// Compare the provided password with the saved hash. Use the bcrypt
+	// comparison function so it is cryptographically secure.
+	if err := bcrypt.CompareHashAndPassword(a.PasswordHash, []byte(password)); err != nil {
+		return auth.Claims{}, ErrAuthenticationFailure
+	}
+
+	// If we are this far the request is valid. Create some claims for the account
+	// and generate the token for the account.
+	claims := auth.NewClaims(a.Id, a.Roles, now, time.Hour)
+	return claims, nil
+}
 
 // Create inserts a new account into the database.
 func Create(ctx context.Context, db *sqlx.DB, n NewAccount, now time.Time) (*Account, error) {
