947669aa291cc4f63552af412954dc0458b8eaf2
deezone <darrendouglas.lee@gmail.com>
2021-01-30 18:04:54 -0500

    Issue-61 update business logic for stations to include account claims lookup for addd and update

--- internal/station_type/station.go
+++ internal/station_type/station.go
@@ -6,17 +6,31 @@ import (
 	"database/sql"
 	"time"
 
+	// Internal packages
+	"github.com/deezone/HydroBytes-BaseStation/internal/platform/auth"
+
 	// Third-party packages
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
 
+// Predefined errors identify expected failure conditions.
+var (
+	// ErrStationNotFound is used when a specific Station is requested but does not exist.
+	ErrStationNotFound = errors.New("station not found")
+
+	// ErrForbidden occurs when an account tries to do something that is forbidden to
+	// it according to our access control policies.
+	ErrForbidden = errors.New("Attempted action is not allowed")
+)
+
 // AddStation adds a station of a specific StationType.
-func AddStation(ctx context.Context, db *sqlx.DB, ns NewStation, stationTypeID string, now time.Time) (*Station, error) {
+func AddStation(ctx context.Context, db *sqlx.DB, account auth.Claims, ns NewStation, stationTypeID string, now time.Time) (*Station, error) {
 	s := Station{
 		Id:            uuid.New().String(),
 		StationTypeId: stationTypeID,
+		AccountId:     account.Subject,
 		Name:          ns.Name,
 		Description:   ns.Description,
 		LocationX:     ns.LocationX,
@@ -26,12 +40,13 @@ func AddStation(ctx context.Context, db *sqlx.DB, ns NewStation, stationTypeID s
 	}
 
 	const q = `INSERT INTO station
-		(id, station_type_id, name, description, location_x, location_y, date_created, date_updated)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+		(id, station_type_id, account_id, name, description, location_x, location_y, date_created, date_updated)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
 
 	_, err := db.ExecContext(ctx, q,
 		s.Id,
 		s.StationTypeId,
+		s.AccountId,
 		s.Name,
 		s.Description,
 		s.LocationX,
@@ -48,10 +63,17 @@ func AddStation(ctx context.Context, db *sqlx.DB, ns NewStation, stationTypeID s
 
 // AdjustStation modifies data about a Station. It will error if the specified ID is
 // invalid or does not reference an existing Station.
-func AdjustStation(ctx context.Context, db *sqlx.DB, id string, update UpdateStation, now time.Time) error {
+func AdjustStation(ctx context.Context, db *sqlx.DB, account auth.Claims, id string, update UpdateStation, now time.Time) error {
 	s, err := RetrieveStation(ctx, db, id)
 	if err != nil {
 		return err
+	}
+
+	// If the account attempt to access does not have the admin role ...
+	// or the owner of the station ...
+	// then restrict access
+	if !account.HasRole(auth.RoleAdmin) && s.AccountId != account.Subject {
+		return ErrForbidden
 	}
 
 	if update.Name != nil {
@@ -113,6 +135,7 @@ func ListStations(ctx context.Context, db *sqlx.DB, stationTypeID string) ([]Sta
       SELECT
         id,
         station_type_id,
+        account_id,
         name, description,
         location_x,
         location_y,
@@ -130,32 +153,33 @@ func ListStations(ctx context.Context, db *sqlx.DB, stationTypeID string) ([]Sta
 
 // Retrieve gets a specific Station from the database.
 func RetrieveStation(ctx context.Context, db *sqlx.DB, id string) (*Station, error) {
-	if _, err := uuid.Parse(id); err != nil {
-		return nil, ErrInvalidID
-	}
+    if _, err := uuid.Parse(id); err != nil {
+        return nil, ErrInvalidID
+    }
 
-	var s Station
+    var s Station
 
-	const q = `
-		SELECT
-			id,
-		    station_type_id,
-			name,
-			description,
-			location_x,
-			location_y,
-			date_created,
-			date_updated
-		FROM station
-		WHERE id = $1`
+    const q = `
+        SELECT
+            id,
+            station_type_id,
+            account_id,
+            name,
+            description,
+            location_x,
+            location_y,
+            date_created,
+            date_updated
+        FROM station
+        WHERE id = $1`
 
-	if err := db.GetContext(ctx, &s, q, id); err != nil {
-		if err == sql.ErrNoRows {
-			return nil, ErrStationNotFound
-		}
+    if err := db.GetContext(ctx, &s, q, id); err != nil {
+        if err == sql.ErrNoRows {
+            return nil, ErrStationNotFound
+        }
 
-		return nil, errors.Wrap(err, "selecting single station")
-	}
+        return nil, errors.Wrap(err, "selecting single station")
+    }
 
-	return &s, nil
+    return &s, nil
 }
